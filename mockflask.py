512a3d630d4d2e5dd16cf10b102398d80d2c4038
chairco <chairco@gmail.com>
2015-08-03 17:08:42 +0800

    change server ip to 0.0.0.0

--- mockflask.py
+++ mockflask.py
@@ -48,4 +48,4 @@ def run_robot():
         return jsonify({'retfaill': [{"fail": ret}]})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
